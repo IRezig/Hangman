6ca5838ddf7e7a169edd94689ef5f6b8cf42697c
ibtissemr <ibtissem@abyssale.com>
2023-03-12 10:58:35 +0100

    add hangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,28 @@
 export function HangmanWord(): JSX.Element {
+  const wordToGuess = "TEST"
+  const guessedLetters = ["T", "E"]
   return (
     <div
       style={{
-        fontSize: "2rem",
-        textAlign: "center",
-      }}></div>
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}>
+      {wordToGuess.split("").map((letter, index) => {
+        return (
+          <span style={{ borderBottom: ".1em solid black" }}>
+            <span
+              key={index}
+              style={{
+                visibility: guessedLetters.includes(letter) ? "visible" : "hidden",
+              }}>
+              {letter}
+            </span>
+          </span>
+        )
+      })}
+    </div>
   )
 }
